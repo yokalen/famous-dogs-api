ffd7b9cda5708c1b28aac0e9c1cd44d3c931f531
My Computer <yokalen@gmail.com>
2022-11-02 17:40:52 -0700

    Add event listener with fetch request

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert('Working')
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const dogName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://famous-dogs-api-fun.herokuapp.com/api/${dogName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.breed
+    }catch(error){
+        console.log(error)
+    }
+}
